9aaaa3bf209f942106ce08702bb2932f47c9b826
Ian-Gm <iangromar@gmail.com>
2024-01-22 14:16:52 -0300

    Add project files.

---
+++ FileOrganizer/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FileOrganizer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<string> FileList { get; set; }
+
+        public MainWindow()
+        {
+            FileList = new ObservableCollection<string>();
+            InitializeComponent();
+            this.DataContext = this;
+
+            string FileListText = "";
+
+            foreach (string file in FileList)
+            {
+                FileListText += file + "\n";
+            }
+
+            TextFileNames.Text = FileListText;
+        }
+
+        private void dropfiles(object sender, System.Windows.DragEventArgs e) //Esta es la función que recibe archivos por drag n drop
+        {
+            string[] droppedFiles = null;
+
+            if (e.Data.GetDataPresent(System.Windows.DataFormats.FileDrop))
+            {
+                droppedFiles = e.Data.GetData(System.Windows.DataFormats.FileDrop, true) as string[];
+            }
+
+            if ((null == droppedFiles) || (!droppedFiles.Any())) { return; }
+
+            foreach (string s in droppedFiles)
+            {
+                string fileName = s;
+
+                FileList.Add(s);
+            }
+        }
+
+        
+    }
+}
