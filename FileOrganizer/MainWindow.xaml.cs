76c0e4c1fcbe2d85753b577ae52e51f8c6538d8d
Ian-Gm <iangromar@gmail.com>
2024-01-22 20:09:41 -0300

    Features
    The excel sheet is read as a DataTable:
    First Column: Keyword
    Second Column: Preset name
    Third Column: Directory
    
    When a file is dropped onto the application they're compaired with the keywords, copied to the corresponding directoy with the corresponding preset name.

--- FileOrganizer/MainWindow.xaml.cs
+++ FileOrganizer/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.Text;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,7 +9,13 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Collections.Generic;
+using ExcelDataReader;
+using System.Data;
+using System.Windows.Media.Media3D;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
 
 namespace FileOrganizer
 {
@@ -17,22 +24,72 @@ namespace FileOrganizer
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public ObservableCollection<string> FileList { get; set; }
+        public Dictionary<string, string> DirectoriesDictionary { get; set; }
+        public Dictionary<string, string> RenamingDictionary { get; set; }
+
+        public DataTable excelData {  get; set; }
 
         public MainWindow()
         {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
             FileList = new ObservableCollection<string>();
+            DirectoriesDictionary = new Dictionary<string, string>();
+            RenamingDictionary = new Dictionary<string, string>();
+            excelData = new DataTable();
+
             InitializeComponent();
             this.DataContext = this;
 
-            string FileListText = "";
+            //READ ALL .TXT FILES
+            string appPath = AppContext.BaseDirectory;
+            string appPathPrevious = Directory.GetParent(appPath).Parent.FullName;
+            string excelPath = Path.Combine(appPathPrevious, @"data.xlsx");
 
-            foreach (string file in FileList)
+            ReadDataExcel(excelPath);
+
+            //string gateDirectory = Path.Combine(appPathPrevious, @"Gate");
+            string gateDirectory = @"C:\Users\Ian\Desktop\Gate";
+
+            using FileSystemWatcher watcher = new FileSystemWatcher(gateDirectory);
+            watcher.NotifyFilter = NotifyFilters.Attributes
+                                 | NotifyFilters.CreationTime
+                                 | NotifyFilters.DirectoryName
+                                 | NotifyFilters.FileName
+                                 | NotifyFilters.LastAccess
+                                 | NotifyFilters.LastWrite
+                                 | NotifyFilters.Security
+                                 | NotifyFilters.Size;
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.EnableRaisingEvents = true;
+
+            string[] gateFiles = Directory.GetFiles(gateDirectory);
+
+            string[] args = Environment.GetCommandLineArgs();
+
+            if(args.Length > 0)
             {
-                FileListText += file + "\n";
+                foreach( string arg in args)
+                {
+                    if(Path.GetExtension(arg) != ".dll")
+                    {
+                        gateFiles.Append(arg);
+                    }
+                }
             }
 
+            AddItemstoFileList(gateFiles);
+
+            string FileListText = "";
+
             TextFileNames.Text = FileListText;
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine(e.FullPath);
         }
 
         private void dropfiles(object sender, System.Windows.DragEventArgs e) //Esta es la función que recibe archivos por drag n drop
@@ -46,14 +103,115 @@ namespace FileOrganizer
 
             if ((null == droppedFiles) || (!droppedFiles.Any())) { return; }
 
-            foreach (string s in droppedFiles)
-            {
-                string fileName = s;
-
-                FileList.Add(s);
-            }
+            AddItemstoFileList(droppedFiles);
         }
 
-        
+        private void ReadDataExcel(string filePath)
+        {
+            if (!Path.Exists(filePath))
+            {
+                return;
+            }
+
+            FileStream stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
+            IExcelDataReader excelReader;
+
+            excelReader = ExcelDataReader.ExcelReaderFactory.CreateReader(stream);
+
+            var conf = new ExcelDataSetConfiguration
+            {
+                ConfigureDataTable = _ => new ExcelDataTableConfiguration
+                {
+                    UseHeaderRow = true
+                }
+            };
+
+            var dataSet = excelReader.AsDataSet(conf);
+
+            excelData = dataSet.Tables[0];
+        }
+
+        private void AddItemstoFileList(string[] files)
+        {
+            foreach (string s in files)
+            {
+                if (!Path.GetFileName(s).EndsWith(".dll"))
+                {
+                    FileList.Add(s);
+                }
+            }
+            OrganizeFiles();
+        }
+
+        private void AddItemtoFileList(string file)
+        {
+            FileList.Add(file);
+
+            OrganizeFiles();
+        }
+
+        private void OrganizeFiles()
+        {
+
+            foreach(string file in FileList)
+            {
+                int rowIndex = 0;
+                string fileName = Path.GetFileNameWithoutExtension(file);
+                string fullName = Path.GetFileName(file);
+                if (fileName.Contains("_"))
+                {
+                    fileName = fileName.Split('_')[0];
+
+                }
+                
+                foreach (DataRow row in excelData.Rows)
+                {
+                    string Keyword = row["Keyword"].ToString();
+
+                    if (fileName.Equals(Keyword))
+                    {
+                        string? filePath = null;
+                        string? rename = null;
+
+                        try
+                        {
+                            filePath = row["Directory"].ToString();
+                        }
+                        catch
+                        {
+
+                        }
+
+                        try
+                        {
+                            rename = row["Preset"].ToString();
+                        }
+                        catch
+                        {
+                        
+                        }
+
+                        string ext = Path.GetExtension(file);
+                        if(filePath != null && rename != null)
+                        {
+                            string newfile = Path.Combine(filePath, rename + ext);
+
+                            try
+                            {
+                                File.Copy(file, newfile);
+                            }
+                            catch(Exception e)
+                            { 
+                            
+                            }
+                        }
+                    }
+
+                    rowIndex++;
+                }
+            }
+
+            FileList.Clear();
+        }
     }
 }
